4bcc287bd447e628255974bfeadca3044ac5d50a
askesian <cwigley@gmail.com>
2015-09-11 00:53:58 -0500

    Adding initial files

---
+++ map.js
@@ -0,0 +1,13 @@
+(function() {
+  var countryElements = document.getElementById('countries').childNodes;
+  var countryCount = countryElements.length;
+  var messageEl = document.querySelector('.message');
+
+  function observeCountryClick(e) {
+    messageEl.innerHTML = e.target.getAttribute('data-name');
+  }
+
+  for (var i = 0; i < countryCount; i++) {
+   countryElements[i].addEventListener("click", observeCountryClick, false);
+  }
+}());
