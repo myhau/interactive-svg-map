a7916efd7698c474421280afd1d3926fdeb692db
askesian <cwigley@gmail.com>
2015-09-12 22:42:18 -0500

    Improve project files strucutre; introduce gulp

---
+++ README.md
@@ -0,0 +1,18 @@
+# Interactive SVG Map
+This project demonstrates how to create an interactive map using SVG as the map source.
+
+## Goals
+The goals of this project are:
+
+* Generate an optimized SVG file for a global map of all countries as individual paths
+* Be able to interact directly with each individual country on the map
+* Be able to interact indirectly with each individual country on the map via interactions with user interface elements that are not a part of the map
+
+## Dependencies
+[TODO:]
+## How to build the map SVG
+[TODO:]
+## How to build the project
+[TODO:]
+## Development
+[TODO:]
---
+++ gulpfile.babel.js
@@ -0,0 +1,58 @@
+import del from 'del';
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import seq from 'gulp-sequence';
+import svgmin from 'gulp-svgmin';
+
+const dirs = {
+  src: 'src',
+  dest: 'dist'
+};
+
+const paths = {
+  sass: {
+    src: `${dirs.src}/public/scss/map.scss`,
+    dest: `${dirs.dest}/public/css/`
+  },
+  js: {
+    src: `${dirs.src}/public/js/**/*.js`,
+    dest: `${dirs.dest}/public/js/`
+  },
+  vectors: {
+    src: `${dirs.src}/public/vectors/**/*.svg`,
+    dest: `${dirs.dest}/public/vectors/`
+  }
+};
+
+gulp.task('build', ['svg', 'sass', 'js']);
+
+gulp.task('serve', (done) => {
+  seq('clean', 'build', 'browserSync', done);
+});
+
+gulp.task('clean', (done) => {
+  del(dirs.dest, done);
+});
+
+gulp.task('svg', () => {
+  let config = paths.vectors;
+
+  return gulp.src(config.src)
+    .pipe(svgmin())
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('sass', () => {
+  let config = paths.sass;
+
+  return gulp.src(config.src)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('js', () => {
+  let config = paths.js;
+
+  return gulp.src(config.src)
+    .pipe(gulp.dest(config.dest));
+});
