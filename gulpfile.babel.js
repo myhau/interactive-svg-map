74d5e0fd433660456edef0048e2672d3363c7413
askesian <cwigley@gmail.com>
2015-09-22 09:54:52 -0500

    Add jshint and jscs; add update watch tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,25 @@
 import autoprefixer from 'autoprefixer';
 import browserSync from 'browser-sync';
+import concat from 'gulp-concat';
 import cssmin from 'cssnano';
 import del from 'del';
 import deploy from 'gulp-gh-pages';
+import download from 'gulp-download';
 import fs from 'fs';
 import gulp from 'gulp';
 import htmlmin from 'gulp-minify-html';
+import jscs from 'gulp-jscs';
+import jshint from 'gulp-jshint';
+import minimatch from 'minimatch';
+import plumber from 'gulp-plumber';
 import postcss from 'gulp-postcss';
+import rename from 'gulp-rename';
 import replace from 'gulp-replace';
 import sass from 'gulp-sass';
 import seq from 'gulp-sequence';
 import svgmin from 'gulp-svgmin';
+import uglify from 'gulp-uglify';
+import unzip from 'gulp-unzip';
 
 const dirs = {
   src: 'src',
@@ -18,6 +27,10 @@ const dirs = {
 };
 
 const paths = {
+  data: {
+    src: `${dirs.src}/data/`,
+    dest: `${dirs.dest}/public/data/`
+  },
   sass: {
     src: `${dirs.src}/public/scss/map.scss`,
     dest: `${dirs.dest}/public/css/`
@@ -65,21 +78,49 @@ gulp.task('sass', () => {
   ];
 
   return gulp.src(config.src)
+    .pipe(plumber())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(postcss(processors))
-    .pipe(gulp.dest(config.dest));
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('js', () => {
+gulp.task('js', ['lint:js', 'js:vendor'], () => {
   let config = paths.js;
 
   return gulp.src(config.src)
+    .pipe(uglify())
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js:vendor', () => {
+  let config = paths.js;
+  let deps = [
+    './node_modules/d3/d3.min.js',
+    './node_modules/d3-geo-projection/d3.geo.projection.min.js',
+    './node_modules/topojson/topojson.min.js'
+  ];
+
+  return gulp.src(deps)
+    .pipe(concat('d3.pack.js'))
     .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('lint:js', () => {
+  let config = paths.js;
+
+  return gulp.src(config.src)
+    .pipe(plumber())
+    .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('insert:map', () => {
   let config = paths.vectors;
-  let svgContent = fs.readFileSync(config.dest + '/map.svg', "utf8");
+  let svgContent = fs.readFileSync(config.dest + '/map.svg', 'utf8');
 
   return gulp.src(dirs.src + '/index.html')
     .pipe(replace(/{{MAP_SVG}}/g, svgContent))
@@ -87,9 +128,53 @@ gulp.task('insert:map', () => {
     .pipe(gulp.dest(dirs.dest));
 });
 
-gulp.task('copy', ['copy:favicon']);
+gulp.task('copy', ['copy:data', 'copy:favicon']);
+
+gulp.task('copy:data', () => {
+  let config = paths.data;
+
+  return gulp.src(config.src + '/*.json')
+    .pipe(gulp.dest(config.dest));
+});
 
 gulp.task('copy:favicon', () => {
   return gulp.src(dirs.src + '/public/favicon.ico')
     .pipe(gulp.dest(dirs.dest));
+});
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: dirs.dest
+    }
+  });
+
+  gulp.watch(paths.sass.src, ['sass']);
+  gulp.watch(paths.js.src, ['js']);
+});
+
+gulp.task('download:mapdata', ['download:countrydata']);
+
+gulp.task('download:countrydata', () => {
+  let url = 'http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries_lakes.zip';
+
+  return download(url)
+    .pipe(unzip({
+      filter: function(file) {
+        return minimatch(file.path, '**/*.{dbf,shp,shx}')
+      }
+    }))
+    .pipe(gulp.dest(paths.data.src));
+});
+
+gulp.task('download:placedata', () => {
+  let url = 'http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_populated_places.zip';
+
+  return download(url)
+    .pipe(unzip({
+      filter: function(file) {
+        return minimatch(file.path, '**/*.{dbf,shp,shx}')
+      }
+    }))
+    .pipe(gulp.dest(paths.data.src));
 });
